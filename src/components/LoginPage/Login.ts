05598608704f1fe6b152d08c234ac38007384b6d
IsabellaAngelC <isabellacuartasvelasquez@gmail.com>
2024-05-05 21:44:55 -0500

    Creé login html y una parte de su componente

--- src/components/LoginPage/Login.ts
+++ src/components/LoginPage/Login.ts
@@ -11,7 +11,16 @@ class login extends HTMLElement {
     render(){
         if(this.shadowRoot){
             this.shadowRoot.innerHTML = ``;
-            
+
+
+            const title = this.ownerDocument.createElement('h1');
+			title.innerText = 'Log in';
+			this.shadowRoot?.appendChild(title);
+
+            const userTitle = this.ownerDocument.createElement('h2');
+			title.innerText = 'Username';
+			this.shadowRoot?.appendChild(title);
+
          const login = this.ownerDocument.createElement( 'login-page');
         this.shadowRoot?.appendChild(login);   
         }
