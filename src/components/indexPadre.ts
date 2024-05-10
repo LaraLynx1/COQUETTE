953a759f2a344a4ed71ab42632d6e67f0b35a10e
IsabellaAngelC <isabellacuartasvelasquez@gmail.com>
2024-05-10 14:44:59 -0500

    arreglo indexA

---
+++ src/components/indexPadre.ts
@@ -0,0 +1,9 @@
+export { default as toFollow } from '../components/mainPage/toFollow/tofollow';
+export { default as cardFollow } from '../components/mainPage/cardFollow/cardfollow';
+export { default as CrearPublicacion } from '../components/mainPage/publication/publicacion';
+export { default as publicWrapper } from '../components/mainPage/publicwrapper/publicwrapper';
+export { default as CrearOpinion } from '../components/mainPage/shareopinion/shareopinion';
+export { default as wrapperOpinion } from '../components/mainPage/opinionwrapper/opinionwrapper';
+export { default as cardInicio } from '../components/mainPage/sidebar/sidebar';
+export { default as cardStories } from '../components/mainPage/cardStories/cardStories';
+export { default as storyCard } from '../components/mainPage/stories/stories';
