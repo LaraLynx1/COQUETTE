152ff57fff92dd1266830005397bf57b833ced04
LaraLynx1 <lauraivettepenaranda@gmail.com>
2024-05-10 13:56:14 -0500

    YA TODO DE MI APRTE LSITO

--- src/components/mainPage/shareopinion/shareopinion.ts
+++ src/components/mainPage/shareopinion/shareopinion.ts
@@ -1,4 +1,8 @@
 import styles from './shareopinion.css';
+import { publics } from '../../../data/data';
+import { PANTALLAS } from '../../../types/enumeraciones';
+import { navigate } from '../../../types/store';
+import { dispatch } from '../../../store/index';
 
 export enum datacosas3 {
 	'userpfp' = 'userpfp',
@@ -6,6 +10,7 @@ export enum datacosas3 {
 
 class Crearopinion extends HTMLElement {
 	userpfp?: string;
+	modal?: HTMLElement;
 
 	constructor() {
 		super();
@@ -31,7 +36,37 @@ class Crearopinion extends HTMLElement {
 
 	connectedCallback() {
 		this.render();
-		this.shadowRoot?.querySelector('#post')?.addEventListener('click', this.onClickPost);
+		//this.shadowRoot?.querySelector('#post')?.addEventListener('click', this.onClickPost);
+
+		const valorInputimagenpost = this.shadowRoot?.querySelector('#imagenpost') as HTMLInputElement;
+		const valorInputusuario = this.shadowRoot?.querySelector('#usuario') as HTMLInputElement;
+		const valorInputusuariopfp = this.shadowRoot?.querySelector('#usuariopfp') as HTMLInputElement;
+		const valorInputmegusta = this.shadowRoot?.querySelector('#megusta') as HTMLInputElement;
+
+		const buttonGuardar = this.shadowRoot?.querySelector('#guardar');
+		buttonGuardar?.addEventListener('click', () => {
+			this.modal!.className! = 'contenedorpost ocultarpost';
+			/* 			console.log('valor del campo', valorInputusuario!.value);
+			console.log('valor del campo', valorInputusuariopfp!.value);
+			console.log('valor del campo', valorInputimagenpost!.value);
+			console.log('valor del campo', valorInputmegusta!.value); */
+			const postData = {
+				user: valorInputusuario!.value,
+				userpfp: valorInputusuariopfp!.value,
+				image: valorInputimagenpost!.value,
+				likes: valorInputmegusta!.value,
+			};
+			publics.push(postData);
+			dispatch(navigate(PANTALLAS.DASHBOARD));
+		});
+
+		const buttonpostear = this.shadowRoot?.querySelector('#post');
+
+		buttonpostear?.addEventListener('click', () => {
+			this.modal!.className! = 'contenedorpost mostrarpost';
+		});
+		//push array de post
+		//publics.push({})
 	}
 
 	attributeChangedCallback(attrName: datacosas3, oldVal: any, newVal: any) {
@@ -60,6 +95,41 @@ class Crearopinion extends HTMLElement {
         </div>
 				<div class="linea"></div>
       `;
+			this.modal = this.ownerDocument.createElement('div');
+			this.modal.className = 'contenedorpost ocultarpost';
+			const formulario = this.ownerDocument.createElement('div');
+			formulario.className = 'formulario';
+			const titulo = this.ownerDocument.createElement('H1');
+			formulario.appendChild(titulo);
+			titulo.innerHTML = 'Datos del Post';
+
+			const usuario = this.ownerDocument.createElement('input');
+			usuario.placeholder = 'usuario';
+			usuario.setAttribute('id', 'usuario');
+			formulario.appendChild(usuario);
+
+			const usuariopfp = this.ownerDocument.createElement('input');
+			usuariopfp.placeholder = 'pfp';
+			usuariopfp.setAttribute('id', 'usuariopfp');
+			formulario.appendChild(usuariopfp);
+
+			const imagenpost = this.ownerDocument.createElement('input');
+			imagenpost.placeholder = 'imagen';
+			imagenpost.setAttribute('id', 'imagenpost');
+			formulario.appendChild(imagenpost);
+
+			const megusta = this.ownerDocument.createElement('input');
+			megusta.placeholder = 'me gustan';
+			megusta.setAttribute('id', 'megusta');
+			formulario.appendChild(megusta);
+
+			const guardar = this.ownerDocument.createElement('button');
+			guardar.setAttribute('id', 'guardar');
+			formulario.appendChild(guardar);
+			guardar.innerHTML = 'SAVE';
+
+			this.modal.appendChild(formulario);
+			this.shadowRoot.appendChild(this.modal);
 		}
 		const cssprofile = this.ownerDocument.createElement('style');
 		cssprofile.innerHTML = styles;
