b058c915cfcd709b6e98e2d512a74c36db0b8944
IsabellaAngelC <isabellacuartasvelasquez@gmail.com>
2024-05-10 10:17:21 -0500

    Firebase comentado

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,17 +1,17 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-import { collection, addDoc, getDocs  } from "firebase/firestore";
+// import { initializeApp } from "firebase/app";
+// import { getFirestore } from "firebase/firestore";
+// import { collection, addDoc, getDocs  } from "firebase/firestore";
 
-const firebaseConfig = {
-    apiKey: "AIzaSyANsoNo49zqX0WQ9mlb3yOFNAc31Lt-dKU",
-    authDomain: "coquette-dcc06.firebaseapp.com",
-    projectId: "coquette-dcc06",
-    storageBucket: "coquette-dcc06.appspot.com",
-    messagingSenderId: "656773814236",
-    appId: "1:656773814236:web:29067ac07200e3368d6119",
-    measurementId: "G-LZBKJP8VGN"
-  };
+// const firebaseConfig = {
+//     apiKey: "AIzaSyANsoNo49zqX0WQ9mlb3yOFNAc31Lt-dKU",
+//     authDomain: "coquette-dcc06.firebaseapp.com",
+//     projectId: "coquette-dcc06",
+//     storageBucket: "coquette-dcc06.appspot.com",
+//     messagingSenderId: "656773814236",
+//     appId: "1:656773814236:web:29067ac07200e3368d6119",
+//     measurementId: "G-LZBKJP8VGN"
+//   };
 
-  const app = initializeApp(firebaseConfig);
+//   const app = initializeApp(firebaseConfig);
 
-const db = getFirestore(app);
+// const db = getFirestore(app);
