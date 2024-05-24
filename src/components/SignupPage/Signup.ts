d3e217d9d15d00e35a633f4b25d1ec947388b934
LaraLynx1 <lauraivettepenaranda@gmail.com>
2024-05-24 11:49:42 -0500

    duque me anda ayudando

--- src/components/SignupPage/Signup.ts
+++ src/components/SignupPage/Signup.ts
@@ -3,142 +3,139 @@ import styles from './Signup.css';
 import { addObserver, dispatch } from '../../store/index';
 import { navigate } from '../../types/store';
 import { PANTALLAS } from '../../types/enumeraciones';
+import { registrarUsuario } from '../../services/firebase';
 
 class SignUpComponent extends HTMLElement {
-    constructor() {
-        super();
-        this.attachShadow({ mode: 'open' });
-        this.render();
-    }
+	constructor() {
+		super();
+		this.attachShadow({ mode: 'open' });
+		this.render();
+	}
 
-    connectedCallback() {
-        this.shadowRoot?.querySelector('form')?.addEventListener('submit', this.onSubmit.bind(this));
+	connectedCallback() {
+		this.shadowRoot?.querySelector('form')?.addEventListener('submit', this.onSubmit.bind(this));
 
-        const button = this.shadowRoot?.querySelector('#botonregreso');
-        button?.addEventListener('click', () => {
-            dispatch(navigate(PANTALLAS.LOGIN));
-        });
-    }
+		const button = this.shadowRoot?.querySelector('#botonregreso');
+		button?.addEventListener('click', () => {
+			dispatch(navigate(PANTALLAS.LOGIN));
+		});
+	}
 
-    disconnectedCallback() {
-        this.shadowRoot?.querySelector('form')?.removeEventListener('submit', this.onSubmit.bind(this));
-    }
+	disconnectedCallback() {
+		this.shadowRoot?.querySelector('form')?.removeEventListener('submit', this.onSubmit.bind(this));
+	}
 
-    async onSubmit(event: Event) {
-        event.preventDefault();
-        const username = (this.shadowRoot?.querySelector('#username') as HTMLInputElement).value;
-        const email = (this.shadowRoot?.querySelector('#email') as HTMLInputElement).value;
-        const birthday = (this.shadowRoot?.querySelector('#birthday') as HTMLInputElement).value;
-        const phone = (this.shadowRoot?.querySelector('#phone') as HTMLInputElement).value;
-        const password = (this.shadowRoot?.querySelector('#password') as HTMLInputElement).value;
+	async onSubmit(event: Event) {
+		event.preventDefault();
+		const username = (this.shadowRoot?.querySelector('#username') as HTMLInputElement).value;
+		const email = (this.shadowRoot?.querySelector('#email') as HTMLInputElement).value;
+		const birthday = (this.shadowRoot?.querySelector('#birthday') as HTMLInputElement).value;
+		const phone = (this.shadowRoot?.querySelector('#phone') as HTMLInputElement).value;
+		const password = (this.shadowRoot?.querySelector('#password') as HTMLInputElement).value;
 
-        console.log(`Username: ${username}, Email: ${email}, Birthday: ${birthday}, Phone: ${phone}, Password: ${password}`);
+		console.log(
+			`Username: ${username}, Email: ${email}, Birthday: ${birthday}, Phone: ${phone}, Password: ${password}`
+		);
+	}
 
-        try {
-            // Registrar al usuario con Firebase Authentication
-            const userCredential = await createUserWithEmailAndPassword(auth, email, password);
-            const user = userCredential.user;
+	render() {
+		if (this.shadowRoot) {
+			this.shadowRoot.innerHTML = '';
 
-            // Guardar datos adicionales en Firestore
-            const userData = {
-                uid: user.uid,
-                username,
-                email,
-                birthday,
-                phone
-            };
-            await addUser(userData);
+			const header = document.createElement('h2');
+			header.textContent = 'Sign up Now';
 
-            console.log("User registered with ID: ", user.uid);
-            // Aquí puedes navegar a otra pantalla si es necesario
-        } catch (e) {
-            console.error("Error registering user: ", e);
-        }
-    }
+			// Create the form
+			const form = document.createElement('form');
 
-    render() {
-        if (this.shadowRoot) {
-            this.shadowRoot.innerHTML = '';
+			// Create username input
+			const usernameInput = document.createElement('input');
+			usernameInput.id = 'username';
+			usernameInput.type = 'text';
+			usernameInput.placeholder = 'Enter your username';
+			form.appendChild(usernameInput);
 
-            const header = document.createElement('h2');
-            header.textContent = 'Sign up Now';
+			// Create email input
+			const emailInput = document.createElement('input');
+			emailInput.id = 'email';
+			emailInput.type = 'email';
+			emailInput.placeholder = 'Enter your email';
+			form.appendChild(emailInput);
 
-            // Create the form
-            const form = document.createElement('form');
+			// Create container for birthday and phone with flex layout
+			const flexContainer = document.createElement('div');
+			flexContainer.style.display = 'flex';
+			flexContainer.style.justifyContent = 'space-between';
 
-            // Create username input
-            const usernameInput = document.createElement('input');
-            usernameInput.id = 'username';
-            usernameInput.type = 'text';
-            usernameInput.placeholder = 'Enter your username';
-            form.appendChild(usernameInput);
+			// Create birthday input
+			const birthdayInput = document.createElement('input');
+			birthdayInput.id = 'birthday';
+			birthdayInput.type = 'date';
+			birthdayInput.style.flex = '1';
+			birthdayInput.style.marginRight = '4px';
+			flexContainer.appendChild(birthdayInput);
 
-            // Create email input
-            const emailInput = document.createElement('input');
-            emailInput.id = 'email';
-            emailInput.type = 'email';
-            emailInput.placeholder = 'Enter your email';
-            form.appendChild(emailInput);
+			// Create phone input
+			const phoneInput = document.createElement('input');
+			phoneInput.id = 'phone';
+			phoneInput.type = 'tel';
+			phoneInput.placeholder = 'Enter your phone number';
+			phoneInput.style.flex = '1';
+			phoneInput.style.marginLeft = '4px';
+			flexContainer.appendChild(phoneInput);
 
-            // Create container for birthday and phone with flex layout
-            const flexContainer = document.createElement('div');
-            flexContainer.style.display = 'flex';
-            flexContainer.style.justifyContent = 'space-between';
+			// Append the flex container to the form
+			form.appendChild(flexContainer);
 
-            // Create birthday input
-            const birthdayInput = document.createElement('input');
-            birthdayInput.id = 'birthday';
-            birthdayInput.type = 'date';
-            birthdayInput.style.flex = '1';
-            birthdayInput.style.marginRight = '4px';
-            flexContainer.appendChild(birthdayInput);
+			// Create password input
+			const passwordInput = document.createElement('input');
+			passwordInput.id = 'password';
+			passwordInput.type = 'password';
+			passwordInput.placeholder = 'Enter your password';
+			form.appendChild(passwordInput);
 
-            // Create phone input
-            const phoneInput = document.createElement('input');
-            phoneInput.id = 'phone';
-            phoneInput.type = 'tel';
-            phoneInput.placeholder = 'Enter your phone number';
-            phoneInput.style.flex = '1';
-            phoneInput.style.marginLeft = '4px';
-            flexContainer.appendChild(phoneInput);
+			// Create submit button
+			const submitButton = document.createElement('button');
+			submitButton.setAttribute('id', 'botonregreso');
+			submitButton.textContent = 'Sign up';
+			submitButton.type = 'submit';
+			form.appendChild(submitButton);
 
-            // Append the flex container to the form
-            form.appendChild(flexContainer);
+			// Create footer links div
+			const footerLinks = document.createElement('div');
+			footerLinks.className = 'footer-links';
+			const accountLink = document.createElement('a');
+			accountLink.href = '#';
+			accountLink.textContent = 'Already have an account?';
+			footerLinks.appendChild(accountLink);
 
-            // Create password input
-            const passwordInput = document.createElement('input');
-            passwordInput.id = 'password';
-            passwordInput.type = 'password';
-            passwordInput.placeholder = 'Enter your password';
-            form.appendChild(passwordInput);
+			// Append the footer links to the form
+			form.appendChild(footerLinks);
 
-            // Create submit button
-            const submitButton = document.createElement('button');
-            submitButton.setAttribute('id', 'botonregreso');
-            submitButton.textContent = 'Sign up';
-            submitButton.type = 'submit';
-            form.appendChild(submitButton);
+			// Append elements to the shadow root
+			this.shadowRoot.appendChild(header);
+			this.shadowRoot.appendChild(form);
 
-            // Create footer links div
-            const footerLinks = document.createElement('div');
-            footerLinks.className = 'footer-links';
-            const accountLink = document.createElement('a');
-            accountLink.href = '#';
-            accountLink.textContent = 'Already have an account?';
-            footerLinks.appendChild(accountLink);
+			// Agregar evento de clic al botón de "crear cuenta"
+			// Agregar evento de clic al botón de "crear cuenta"
+			submitButton.addEventListener('click', async () => {
+				const userId = await registrarUsuario(
+					usernameInput.value,
+					parseInt(emailInput.value),
+					passwordInput.value,
+					passwordInput.value
+				);
+				alert(`Usuario registrado con id: ${userId}`);
+				dispatch(navigate(PANTALLAS.DASHBOARD));
+			});
+			console.log('Se hizo clic en el botón de "crear cuenta"');
+		}
 
-            // Append the footer links to the form
-            form.appendChild(footerLinks);
-
-            // Append elements to the shadow root
-            this.shadowRoot.appendChild(header);
-            this.shadowRoot.appendChild(form);
-
-            const cssSignup = document.createElement('style');
-            cssSignup.textContent = styles;
-            this.shadowRoot.appendChild(cssSignup);
-        }
-    }
+		const cssSignup = document.createElement('style');
+		cssSignup.textContent = styles;
+		this.shadowRoot?.appendChild(cssSignup);
+	}
 }
+
 window.customElements.define('sign-up-component', SignUpComponent);
 export default SignUpComponent;
--- webpack.config.js
+++ webpack.config.js
@@ -23,4 +23,5 @@ module.exports = {
 		filename: 'bundle.js',
 		path: path.resolve(__dirname, 'dist'),
 	},
+	performance: { hints: false, maxEntrypointSize: 512000, maxAssetSize: 512000 },
 };
